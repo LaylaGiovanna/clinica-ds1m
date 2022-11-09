0f55c51d4f2f770d4dcc2fa9a5512974fd35effb
LAYLA GIOVANNA FURTADO DA SILVA <22282096@CFP127ALUNO.LOCAL>
2022-11-09 07:45:46 -0300

    Adicionando as alterações da outra aula

---
+++ src/br/senai/sp/jandira/model/Pessoa.java
@@ -0,0 +1,13 @@
+
+package br.senai.sp.jandira.model;
+
+import java.time.LocalDate;
+
+public class Pessoa {
+    
+    private String nome;
+    private String email;
+    private String telefone;
+    private LocalDate dataDeNascimento;
+    
+}
